cf883b6a1775db6aa2d2a228eefe5aef01a8e8fc
awwerbro <andreaswerbrouck@gmail.com>
2020-11-18 23:59:17 +0100

    carousel, bio, some layout fixes

---
+++ _site/en/docs/fr/public/js/custom.js
@@ -0,0 +1,36 @@
+// Closes the sidebar menu
+$("#menu-close").click(function (e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Opens the sidebar menu
+$("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Scrolls to the selected menu item on the page
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') || location.hostname === this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+// Fade out some stuff on scrolling
+$(window).scroll(function () {
+    let top_section_height = $('#top').height();
+    if ($(window).scrollTop() < top_section_height) {
+        $("#header-arrow-down").css("opacity", 1 - $(window).scrollTop() / top_section_height);
+    }
+});
